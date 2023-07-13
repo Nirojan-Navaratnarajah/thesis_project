ee035b844cc00b2bb1aafdedc84643b0e6ec1061
nirojan-sys <68774552+nirojan-sys@users.noreply.github.com>
2023-07-14 00:27:49 +0200

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import threading
 # Create GUI
 window = Tk()
 window.title("HV/DCDC ADC Parameters")
-window.geometry("800x600")
+window.geometry("600x450")
 
 # Create a frame for the FSI data
 fsi_frame = Frame(window, bd=1, relief=SOLID)
@@ -21,33 +21,36 @@ left_frame = Frame(fsi_frame)
 left_frame.pack(side=LEFT, padx=10, pady=10)
 
 # Create a Frame for the right side
-right_frame = Frame(fsi_frame)
+right_frame = Frame(window)
 right_frame.pack(side=LEFT, padx=10, pady=10)
 
-# Create text boxes and labels in the left side
-text_boxes_left = {
-    "Current_LV_Phase1_u16": Entry(left_frame, width=20, state="readonly"),
-    "Current_LV_Phase2_u16": Entry(left_frame, width=20, state="readonly"),
-    "Current_LV_u16": Entry(left_frame, width=20, state="readonly"),
-    "Voltage_LV_u16": Entry(left_frame, width=20, state="readonly"),
-    "Voltage_VClamp_u16": Entry(left_frame, width=20, state="readonly"),
-    "Voltage_HV_u16": Entry(left_frame, width=20, state="readonly"),
-    "Current_HV_u16": Entry(left_frame, width=20, state="readonly")
+# Create labels and text boxes in the left side
+labels_left = {
+    "Current_LV_Phase1_u16": Label(left_frame, text="Current_LV_Phase1_u16: ", anchor=W),
+    "Current_LV_Phase2_u16": Label(left_frame, text="Current_LV_Phase2_u16: ", anchor=W),
+    "Current_LV_u16": Label(left_frame, text="Current_LV_u16: ", anchor=W),
+    "Voltage_LV_u16": Label(left_frame, text="Voltage_LV_u16: ", anchor=W),
+    "Voltage_VClamp_u16": Label(left_frame, text="Voltage_VClamp_u16: ", anchor=W),
+    "Voltage_HV_u16": Label(left_frame, text="Voltage_HV_u16: ", anchor=W),
+    "Current_HV_u16": Label(left_frame, text="Current_HV_u16: ", anchor=W)
 }
 
-labels_left = {}
+text_boxes_left = {}
 
-for i, (key, text_box) in enumerate(text_boxes_left.items()):
-    labels_left[key] = Label(left_frame, text=key + ": ", anchor=W)
-    labels_left[key].grid(row=i, column=0, sticky=W)
-    text_box.grid(row=i, column=1, sticky=W)
-    text_box.insert(END, "No Data Available")
+for i, (key, label) in enumerate(labels_left.items()):
+    label.grid(row=i, column=0, sticky=W, padx=5, pady=5)
+    text_box = Entry(left_frame, width=20)
+    text_box.grid(row=i, column=1, sticky=W, padx=5, pady=5)
+    text_boxes_left[key] = text_box
 
-# Create raw data text boxes in the right side
-raw_text_boxes_left = {
-    key: Entry(right_frame, width=20, state="readonly") for key in text_boxes_left.keys()
-}
+speed_label = Label(left_frame, text="Speed: ", anchor=CENTER)
+speed_label.grid(row=len(labels_left), columnspan=2, padx=5, pady=10)
 
+packet_count = 0
+start_time = 0
+payload = b""  # Initialize payload variable
+
+# Scaling Factors
 scaling_factors = {
     "Current_LV_Phase1_u16": {"lower_limit": 0, "upper_limit": 0},
     "Current_LV_Phase2_u16": {"lower_limit": 0, "upper_limit": 0},
@@ -58,16 +61,44 @@ scaling_factors = {
     "Current_HV_u16": {"lower_limit": -14.285714, "upper_limit": 14.285714, "resolution": 0.00697715, "offset": 2047.5}
 }
 
-# Adjust row heights to increase vertical space
-for i in range(len(text_boxes_left)):
-    right_frame.rowconfigure(i, minsize=30)
+# Create a frame for the Ethernet Frame Sender
+ethernet_frame_sender_frame = Frame(right_frame, bd=1, relief=SOLID)
+ethernet_frame_sender_frame.pack(padx=10, pady=10)
 
-speed_label = Label(left_frame, text="Speed: ", anchor=CENTER)
-speed_label.grid(row=len(text_boxes_left), columnspan=2, sticky=W+E)
+# Create a label for the Ethernet Frame Sender
+ethernet_frame_sender_label = Label(ethernet_frame_sender_frame, text="Ethernet Frame Sender", font=("Arial", 14, "bold"))
+ethernet_frame_sender_label.pack(side=TOP)
 
-packet_count = 0
-start_time = 0
-payload = b""  # Initialize payload variable
+# Create labels and text boxes for data entry
+data_labels = []
+data_entries = []
+
+for i in range(1, 5):
+    data_label = Label(ethernet_frame_sender_frame, text="Data_{}".format(i))
+    data_label.pack(anchor=W)
+    data_labels.append(data_label)
+
+    data_entry = Entry(ethernet_frame_sender_frame, width=30)
+    data_entry.pack(pady=5)
+    data_entries.append(data_entry)
+
+# Create a function to send a single Ethernet frame
+def send_single_frame():
+    dst_mac = "04:03:02:01:06:05"  # Destination MAC address
+    src_mac = "00:73:41:00:04:f8"  # Source MAC address
+    ethertype = 0xffff  # EtherType (0xffff)
+
+    payload = b""
+    for i in range(4):
+        data = data_entries[i].get().encode()[:32]  # Get the data from the entry widget and limit it to 32 bytes
+        payload += data if data else b"\x00" * 32  # Use zero value if no data is entered
+
+    packet = Ether(dst=dst_mac, src=src_mac, type=ethertype) / Raw(load=payload)
+    sendp(packet, iface="Ethernet")  # Replace "Ethernet" with your network interface
+
+# Create a button to trigger sending a single frame
+send_button = Button(ethernet_frame_sender_frame, text="Send Frame", command=send_single_frame)
+send_button.pack(pady=10)
 
 # Function to update the GUI
 def update_gui():
@@ -93,30 +124,16 @@ def process_packet(packet):
         payload = packet.getlayer(Raw).load
         data = [payload[i:i + 2][::-1] for i in range(0, len(payload), 2)]  # Note byte order switching
 
-        for i, (key, text_box, raw_text_box) in enumerate(zip(text_boxes_left.keys(), text_boxes_left.values(), raw_text_boxes_left.values())):
+        for i, (key, text_box) in enumerate(text_boxes_left.items()):
             if key in scaling_factors and i < len(data):
                 scaling_factor = scaling_factors[key]
                 hex_value = int.from_bytes(data[i], byteorder="big", signed=False)
                 scaled_value = ((hex_value - scaling_factor.get("offset", 0)) * scaling_factor.get("resolution", 1.0))
 
-                text_box.config(state="normal")
                 text_box.delete(0, END)
                 text_box.insert(END, "{:.2f}".format(scaled_value))
-                text_box.config(state="readonly")
-
-                raw_text_box.config(state="normal")
-                raw_text_box.delete(0, END)
-                raw_text_box.insert(END, "0x{:04x}".format(hex_value))
-                raw_text_box.config(state="readonly")
             else:
-                text_box.config(state="normal")
                 text_box.delete(0, END)
-                text_box.insert(END, "No Data Available")
-                text_box.config(state="readonly")
-
-                raw_text_box.config(state="normal")
-                raw_text_box.delete(0, END)
-                raw_text_box.config(state="readonly")
 
         packet_count += 1
 
